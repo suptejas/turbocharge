d857dcfb6e81c20026ed0338f091853fba48ed67
TheBossProSniper <thebossprosniper@gmail.com>
2020-09-23 16:38:39 +0530

    Added Many Packages

--- src/constants.py
+++ src/constants.py
@@ -23,7 +23,23 @@ applications = {
     'goland': 'Go Land',
     'rubymine': 'RubyMine',
     'figma-linux': 'Figma',
-    'slack': 'Slack',   
+    'slack': 'Slack',
+    'obs-studio': 'OBS Studio',
+    'postman': 'Postman',
+    'flutter': 'Flutter',
+    'inkscape': 'Inkscape',
+    'gimp': 'GIMP',
+    '1password-linux': '1Password',
+    'skype': 'Skype',
+    'todoist': 'Todoist',
+    'spotify': 'Spotify',
+    'beekeeper-studio': 'BeeKeeper-Studio',
+    'notepad-plus-plus': 'Notepad++',
+    'gitkraken': 'Gitkraken',
+    'cacher': 'Cacher',
+    'space': 'Space',
+    'intellij-idea-educational': 'IntelliJ Idea Educational',
+    'phpstorm': 'PHPStorm',
 }
 
 devpackages = {
@@ -49,7 +65,12 @@ devpackages = {
     'kakoune': 'Kakoune',
     'autojump': 'Autojump',
     'pass': 'Pass',
-    'qtpass': 'QTpass'
+    'qtpass': 'QTpass',
+    'ruby-full': 'Ruby',
+    'julia': 'Julia',
+    'librecad': 'LibreCAD',
+    'krita': 'Krita',
+    'chromium-browser': 'Chromium',
 }
 
 hyperpkgs = {
@@ -81,64 +102,86 @@ miniconda_remove_file = 'rm ~/miniconda.sh'
 
 
 display_list = '''
-__________________________________________
-| Applications      |Duration||   Size   |
-------------------------------------------
-| android-studio    |   5m   || 840.0 MB |
-| atom              |   2m   || 224.8 MB |
-| blender           |   2m   || 187.7 MB |
-| brackets          |   1m   || 109.6 MB |
-| clion             |   3m   || 502.0 MB |
-| chrome            |   2m   || 70.2  MB |
-| datagrip          |   2m   || 356.8 MB |
-| discord           |   1m   || 60.1  MB |
-| eclipse           |   2m   || 220.3 MB |
-| figma-linux       |   2m   || 96.4  MB |
-| libreoffice       |   1m   || 25.0  MB |
-| librepcb          |   1m   || 98.8  MB |
-| opera             |   1m   || 64.2 MB  |
-| pycharm           |   3m   || 372.1 MB |
-| powershell        |   1m   || 62.5  MB |
-| rubymine          |   3m   || 363.2 MB |
-| sublime-text      |   1m   || 70.8 MB  |
-| webstorm          |   3m   || 343.8 MB |
-| vscode            |   2m   || 162.5 MB |
-| vscode-insiders   |   2m   || 153.3 MB |
-------------------------------------------
-________________
-| Package      |
-----------------
-|  anaconda    |
-|  autojump    |
-|  curl        |
-|  docker      |
-|  emacs       |
-|  git         |
-|  golang      |
-|  htop        |
-|  jq          |
-|  kotlin      |
-|  kakoune     |
-|  miniconda   |
-|  ncdu        |
-|  neovim      |
-|  npm         |
-|  pass        |
-|  patchelf    |
-|  qtpass      |
-|  rust        |
-|  sqlite      |
-|  taskwarrior |
-|  tldr        |
-|  tmux        |
-|  vim         |
-|  zsh         |
-|  zlib        |
-----------------
-_______________________________________________________________________
-| HyperPacks  |  Content                                              |
------------------------------------------------------------------------
-|  essential  |  git, curl, npm, zsh, vim, code, atom, sublime-text   |
-|  office     |  sqlite, libreoffice                                  |
------------------------------------------------------------------------
+_______________________________________________________
+| Applications                   |Duration||   Size   |
+-------------------------------------------------------
+| 1password-linux                |   1m   || 114.9 MB |
+| android-studio                 |   5m   || 840.0 MB |
+| atom                           |   2m   || 224.8 MB |
+| beekeeper-studio               |   1m   || 79.1  MB |
+| blender                        |   2m   || 187.7 MB |
+| brackets                       |   1m   || 109.6 MB |
+| cacher                         |   1m   || 73.8  MB |
+| clion                          |   3m   || 502.0 MB |
+| chromium                       |   1m   || 169.9 MB |
+| chrome                         |   1m   || 70.2  MB |
+| datagrip                       |   2m   || 356.8 MB |
+| discord                        |   1m   || 60.1  MB |
+| eclipse                        |   2m   || 220.3 MB |
+| figma-linux                    |   2m   || 96.4  MB |
+| gimp                           |   2m   || 263.2 MB |
+| gitkraken                      |   1m   || 171.2 MB |
+| inkscape                       |   1m   || 14.9  MB |
+| intellij-idea-educational      |   1m   || 14.9  MB |
+| krita                          |   1m   || 147.0 MB |
+| libreoffice                    |   1m   || 25.0  MB |
+| librecad                       |   1m   || 2.6   MB |
+| librepcb                       |   1m   || 98.8  MB |
+| notepad++                      |   1m   || 79.1  MB |
+| obs-studio                     |   1m   || 179.2 MB |
+| opera                          |   1m   || 64.2 MB  |
+| pycharm                        |   3m   || 372.1 MB |
+| postman                        |   1m   || 184.8 MB |
+| phpstorm                       |   3m   || 373.9 MB |
+| powershell                     |   1m   || 62.5  MB |
+| rubymine                       |   3m   || 363.2 MB |
+| space                          |   1m   || 67.1  MB |
+| spotify                        |   1m   || 171.6 MB |
+| slack                          |   1m   || 144.1 MB |
+| sublime-text                   |   1m   || 70.8  MB |
+| skype                          |   1m   || 188.2 MB |
+| todoist                        |   1m   || 66.2  MB |
+| webstorm                       |   3m   || 343.8 MB |
+| vscode                         |   2m   || 162.5 MB |
+| vscode-insiders                |   2m   || 153.3 MB |
+-------------------------------------------------------
+_____________________
+| Package           |
+---------------------
+|  anaconda         |
+|  autojump         |
+|  curl             |
+|  docker           |
+|  emacs            |
+|  flutter          |
+|  git              |
+|  golang           |
+|  htop             |
+|  julia            |
+|  jq               |
+|  kotlin           |
+|  kakoune          |
+|  miniconda        |
+|  ncdu             |
+|  neovim           |
+|  npm              |
+|  pass             |
+|  patchelf         |
+|  qtpass           |
+|  ruby             |
+|  rust             |
+|  sqlite           |
+|  taskwarrior      |
+|  tldr             |
+|  tmux             |
+|  vim              |
+|  zsh              |
+|  zlib             |
+---------------------
+_________________________________________________________________________________
+| HyperPacks  |  Content                                                        |
+---------------------------------------------------------------------------------
+|  essential  |  git, curl, npm, zsh, vim, code, atom, sublime-text             |
+|  office     |  sqlite, libreoffice                                            |
+---------------------------------------------------------------------------------
 '''
