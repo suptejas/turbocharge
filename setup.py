01d787af3bcf4eccb711db3c6ed799b9cd55f388
Erick <erick.8bld@gmail.com>
2020-09-27 12:35:51 +0300

    cleaned post install

--- setup.py
+++ setup.py
@@ -5,42 +5,22 @@ from getpass import getuser
 import os
 from sys import platform
 import subprocess
+from src.Setup import Setup
+import atexit
 
 user = getuser()
 
+s = Setup()
 
-class PostDevelopCommand(develop):
+class PostDevelop(develop):
     def run(self):
         develop.run(self)
-
-        if platform == 'linux':
-            os.system(f'export PATH="/home/{user}/.local/bin:$PATH"')
-
-        elif platform == 'win32':
-            subprocess.Popen(
-                [
-                    'powershell.exe',
-                    'Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))'
-                ]
-            )
-        
-
-class PostInstallCommand(install):
+        s.setup()
+    
+class PostInstall(install):
     def run(self):
         install.run(self)
-
-        if platform == 'linux':
-            os.system(f'export PATH="/home/{user}/.local/bin:$PATH"')
-
-        elif platform == 'win32':
-            subprocess.Popen(
-                [
-                    'powershell.exe',
-                    'Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))'
-                ]
-            )
-
-
+        s.setup()
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -61,6 +41,10 @@ setup(
         'Click',
         'progress',
     ],
+    cmdclass={
+        'develop': PostDevelop,
+        'install': PostInstall
+    },
     package_dir={'': 'src'},
     entry_points = 
     '''
@@ -68,7 +52,7 @@ setup(
         turbo=turbocharge:cli
     ''',
     classifiers=[
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache License 2.0",
         "Operating System :: Unix",
     ]
 )
